27870f7438bcd18572f43dc429ea1110fc3d006e
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-17 19:38:42 -0500

    Done.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function listIterate(){
+  return $('li').map(function(index, item){
+    return item.innerHTML;
+  });
+}
+
 $(document).ready(function(){
-
-
+  $('div').each(function(index, div){
+  $(this).append("this is div number " + (index + 1));
+  });
 });
